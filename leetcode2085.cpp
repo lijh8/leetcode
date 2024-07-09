ddf794fe7cc7c796c53906f8b331fee8a6786b1c
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-07-09 15:12:49 +0800

    Update leetcode2085.cpp

--- leetcode2085.cpp
+++ leetcode2085.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
+    std::vector<std::pair<std::string, size_t>> helper(vector<string>& v) {
+        std::map<std::string, size_t> map;
+        for (auto &e: v){ map[std::move(e)]++; }
+        std::vector<std::pair<std::string, size_t>> t(begin(map), end(map));
+        erase_if(t, [](const auto &e){ return e.second > 1; });
+        return t;
+    }
+
     int countWords(vector<string>& words1, vector<string>& words2) {
-        std::vector<std::string> v;
-        std::map<std::string, size_t> map1;
-        std::map<std::string, size_t> map2;
-        for (const auto &e: words1){ map1[e]++; }
-        for (const auto &e: words2){ map2[e]++; }
-        std::vector<std::pair<std::string, size_t>> v1(begin(map1), end(map1));
-        std::vector<std::pair<std::string, size_t>> v2(begin(map2), end(map2));
-        erase_if(v1, [](const auto &e){ return e.second > 1; });
-        erase_if(v2, [](const auto &e){ return e.second > 1; });
+        auto v1 = helper(words1);
+        auto v2 = helper(words2);
         std::vector<std::pair<std::string, size_t>> v3;
         set_intersection(begin(v1), end(v1), begin(v2), end(v2),
             back_inserter(v3), [](const auto &a, const auto &b){
@@ -17,7 +18,7 @@ public:
             });
         std::vector<std::string> v4;
         for (const auto &e: v3){
-            v4.emplace_back(e.first);
+            v4.emplace_back(std::move(e.first));
         }
         return v4.size();
     }
