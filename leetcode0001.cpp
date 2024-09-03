9d86e405efbf5d4003feb04684d7b64526bf90fd
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-09-04 00:08:00 +0800

    Update leetcode0001.cpp

--- leetcode0001.cpp
+++ leetcode0001.cpp
@@ -1,16 +1,12 @@
 #include <vector>
 
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    int n = nums.size();
-    for (int i = 0; i != n - 1; i++)
-    {
-        for (int j = i + 1; j != n; j++)
-        {
-            if (nums[i] + nums[j] == target)
-            {
-                return {i, j};
-            }
-        }
+std::vector<int> twoSum(std::vector<int> &nums, int target) {
+  for (auto i = 0; i != nums.size() - 1; i++) {
+    for (auto j = i + 1; j != nums.size(); j++) {
+      if (nums[i] + nums[j] == target) {
+        return {i, j};
+      }
     }
-    return {};
+  }
+  return {};
 }
