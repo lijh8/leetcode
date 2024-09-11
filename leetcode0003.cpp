bccdf8df09300e950dd64db3851b88b48701a779
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-09-11 09:53:49 +0800

    Update leetcode0003.cpp

--- leetcode0003.cpp
+++ leetcode0003.cpp
@@ -1,15 +1,32 @@
 #include <string>
 
-  size_t lengthOfLongestSubstring(std::string s) {
+class Solution {
+public:
+  /*
+  https://leetcode.cn/problems/longest-substring-without-repeating-characters/solutions/227999/wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2/comments/2347374
+  */
+
+  size_t lengthOfLongestSubstring(const std::string &s) {
+    std::unordered_map<char, std::string::const_iterator> map;
+    long max = 0;
+    for (auto i = begin(s), j = i; j != end(s); ++j) {
+      if (auto k = map.find(*j); k != end(map) && k->second >= i) {
+        i = k->second + 1;
+      }
+      map[*j] = j;
+      max = std::max(max, j - i + 1);
+    }
+    return max;
+  }
+
+  size_t lengthOfLongestSubstring2(const std::string &s) {
     if (size(s) == 0) {
       return 0;
     }
-    auto first = begin(s);
-    auto last = begin(s);
-    for (auto i = begin(s); i != end(s) - 1; i++) {
-      for (auto j = i + 1; j != end(s); j++) {
-        auto k = find(i, j, *j);
-        if (k != j) {
+    auto first = begin(s), last = first;
+    for (auto i = begin(s); i != end(s) - 1; ++i) {
+      for (auto j = i + 1; j != end(s); ++j) {
+        if (auto k = find(i, j, *j); k != j) {
           i = k;
           break;
         }
@@ -21,24 +38,33 @@
     }
     return last - first + 1;
   }
+};
 
 #include <iostream>
-int main(){
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "abc") << "\n";
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring("ababc") << "\n";
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring("ababcab") << "\n";
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "abcab") << "\n";
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "abcabcbb") << "\n"; // abc
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring("bbbbb") << "\n"; // b
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring("pwwkew") << "\n"; // wke
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring("a") << "\n"; // a
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(" ") << "\n";
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring("") << "\n";
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "abcabcbbabcabcabcd") << "\n"; // abcd
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "aab") << "\n"; // ab
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "dcbaaabcdef") << "\n"; //
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "dcbaafedcba") << "\n"; //
-    std::cout << __LINE__ << ": " << lengthOfLongestSubstring(  "dcbaaabcdef") << "\n"; // abcdef
+int main() {
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("abc") << "\n";
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("ababc") << "\n";
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("ababcab") << "\n";
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("abcab") << "\n";
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("abcabcbb")
+            << "\n"; // abc
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("bbbbb")
+            << "\n"; // b
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("pwwkew")
+            << "\n";                                                      // wke
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("a") << "\n"; // a
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring(" ") << "\n";
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("") << "\n";
+  std::cout << __LINE__ << ": "
+            << lengthOfLongestSubstring("abcabcbbabcabcabcd") << "\n"; // abcd
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("aab")
+            << "\n"; // ab
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("dcbaaabcdef")
+            << "\n"; //
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("dcbaafedcba")
+            << "\n"; //
+  std::cout << __LINE__ << ": " << lengthOfLongestSubstring("dcbaaabcdef")
+            << "\n"; // abcdef
 
-    return 0;
+  return 0;
 }
